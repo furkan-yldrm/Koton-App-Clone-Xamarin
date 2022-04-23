86ecc13fbce418999c6c578279724cf8e5330a06
furkan-yldrm <yildirim.frkn@outlook.com>
2022-04-23 17:32:51 +0300

    Views added. HomePage is ok.

--- KotonAppXamarin/KotonAppXamarin/Views/Account.xaml.cs
+++ KotonAppXamarin/KotonAppXamarin/Views/Account.xaml.cs
@@ -1,21 +1,20 @@
-﻿using KotonAppXamarin.ViewModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace KotonAppXamarin.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class LoginPage : ContentPage
+    public partial class Account : ContentPage
     {
-        public LoginPage()
+        public Account()
         {
             InitializeComponent();
-            this.BindingContext = new LoginViewModel();
         }
     }
 }
