ffd76c0f3df213f138f003105c10dd102ea892ec
furkan-yldrm <yildirim.frkn@outlook.com>
2022-04-26 13:58:48 +0300

    Item details ok

--- KotonAppXamarin/KotonAppXamarin/Views/ItemDetails.xaml.cs
+++ KotonAppXamarin/KotonAppXamarin/Views/ItemDetails.xaml.cs
@@ -12,9 +12,39 @@ namespace KotonAppXamarin.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ItemDetails : ContentPage
     {
+        /* public class ImageInformation
+         {
+             public ImageSource _Image { get; set; }
+         }
+         private ObservableCollection<ImageInformation> imageCollection;
+
+         public ObservableCollection<ImageInformation> ImageCollection
+         {
+             get { return imageCollection; }
+             set
+             {
+                 imageCollection = value;
+                 OnPropertyChanged();
+             }
+         }*/
         public ItemDetails()
         {
             InitializeComponent();
+            BindingContext = this;
+
+        }
+        public class Detay
+        {
+            public string Image { get; set; }
+            public string Aciklama { get; set; }
+        }
+        public List<Detay> Collection { get => ProductList(); }
+
+        private List<Detay> ProductList()
+        {
+            var item = new List<Detay>();
+            item.Add(new Detay { Image = "detailUrun1.png", Aciklama = "REGULAR FIT, BASIC TİŞÖRT" });
+            return item;
         }
     }
 }
