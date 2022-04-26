fb2b49d65d1ac22375181edbcb77546a2dae4fff
furkan-yldrm <yildirim.frkn@outlook.com>
2022-04-26 16:43:40 +0300

    Account login register

--- KotonAppXamarin/KotonAppXamarin/Views/Account.xaml.cs
+++ KotonAppXamarin/KotonAppXamarin/Views/Account.xaml.cs
@@ -16,5 +16,15 @@ namespace KotonAppXamarin.Views
         {
             InitializeComponent();
         }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Register());
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Login());
+        }
     }
 }
