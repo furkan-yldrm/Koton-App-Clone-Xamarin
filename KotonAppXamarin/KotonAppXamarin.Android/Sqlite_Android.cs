4a7b1a061e79ba362980ed1a50cdf058b13b1bf3
furkan-yldrm <yildirim.frkn@outlook.com>
2022-04-27 17:34:02 +0300

    sql deneme

---
+++ KotonAppXamarin/KotonAppXamarin.Android/Sqlite_Android.cs
@@ -0,0 +1,26 @@
+﻿using KotonAppXamarin.Models;
+using Microsoft.Data.Sqlite;
+using System;
+using System.IO;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(MediaMarkt.Droid.SQLite_Android))]
+namespace MediaMarkt.Droid
+{  
+    public class SQLite_Android : ISQLite
+    {
+        public SqliteConnection GetConnection()
+        {
+            var sqliteFileName = "MyDatabase.db";
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            var path = Path.Combine(documentsPath, sqliteFileName);
+            var cn = new SqliteConnection(path);
+            return cn;
+        }
+
+        SqliteConnection ISQLite.GetConnection()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ KotonAppXamarin/KotonAppXamarin/Models/ISQLite.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KotonAppXamarin.Models
+{   
+        public interface ISQLite
+        {
+            SqliteConnection GetConnection();
+        }   
+}
